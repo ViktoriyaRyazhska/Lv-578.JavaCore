2f37f3bbe14478edd9318891f53a1c08f171f341
Your Name <you@example.com>
2021-03-05 20:31:28 +0200

    hw1

---
+++ hw1/sasha4.java
@@ -0,0 +1,20 @@
+package sasha;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sasha4 {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	System.out.println("What is your name?");
+	String name = br.readLine();
+	System.out.println("Where are you live, (name)?");
+	String city = br.readLine();
+
+	System.out.println("Name " + name);  
+	System.out.println("City " + city
+			);
+	  }
+}
