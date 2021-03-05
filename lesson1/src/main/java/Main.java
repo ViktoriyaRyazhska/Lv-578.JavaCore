f30dbed4127d99cf91334c834af14042b66b3216
Petro Feriy <petroferiy@gmail.com>
2021-03-05 18:58:23 +0200

    howe work1

---
+++ lesson1/src/main/java/Main.java
@@ -0,0 +1,46 @@
+import lesson.LessonOne;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        choseTaskFromLessonOne();
+    }
+
+    private static void choseTaskFromLessonOne() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Яке Ви хочете запустити завдання?");
+        int chose = sc.nextInt();
+        switch (chose) {
+            case 1: {
+                LessonOne.task1();
+                break;
+            }
+            case 2: {
+                LessonOne.task2();
+                break;
+            }
+            case 3: {
+                LessonOne.task3();
+                break;
+            }
+            case 4: {
+                LessonOne.task4();
+                break;
+            }
+            case 5: {
+                LessonOne.task5();
+                break;
+            }
+            case 6: {
+                LessonOne.task6();
+                break;
+            }
+            default:
+                System.out.println("Введений неправильний номер завдання");
+        }
+    }
+}
+
