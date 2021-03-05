d37e50a4b94dd2fd91c37ce840f4651c67e07212
Petro Feriy <petroferiy@gmail.com>
2021-03-05 19:27:00 +0200

    Home work 1

--- lesson1/src/main/java/Main.java
+++ lesson1/src/main/java/Main.java
@@ -11,35 +11,37 @@ public class Main {
 
     private static void choseTaskFromLessonOne() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Яке Ви хочете запустити завдання?");
-        int chose = sc.nextInt();
-        switch (chose) {
-            case 1: {
-                LessonOne.task1();
-                break;
+        while (true) {
+            System.out.println("Яке завдання Ви хочете запустити ?");
+            int chose = sc.nextInt();
+            switch (chose) {
+                case 1: {
+                    LessonOne.task1();
+                    break;
+                }
+                case 2: {
+                    LessonOne.task2();
+                    break;
+                }
+                case 3: {
+                    LessonOne.task3();
+                    break;
+                }
+                case 4: {
+                    LessonOne.task4();
+                    break;
+                }
+                case 5: {
+                    LessonOne.task5();
+                    break;
+                }
+                case 6: {
+                    LessonOne.task6();
+                    break;
+                }
+                default:
+                    System.out.println("Введений неправильний номер завдання");
             }
-            case 2: {
-                LessonOne.task2();
-                break;
-            }
-            case 3: {
-                LessonOne.task3();
-                break;
-            }
-            case 4: {
-                LessonOne.task4();
-                break;
-            }
-            case 5: {
-                LessonOne.task5();
-                break;
-            }
-            case 6: {
-                LessonOne.task6();
-                break;
-            }
-            default:
-                System.out.println("Введений неправильний номер завдання");
         }
     }
 }
