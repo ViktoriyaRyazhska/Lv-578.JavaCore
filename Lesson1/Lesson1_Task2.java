defd0ed3fea930b7631ea40e4c948c69da978966
unknown <evvapavlous2001@gmail.com>
2021-03-08 16:08:22 +0200

    added Lesson1_Task2

---
+++ Lesson1/Lesson1_Task2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Lesson1_Task2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How are you?");
+        String firstAnswer = sc.nextLine();
+        System.out.println("Why " + firstAnswer + "?");
+
+    }
+}
