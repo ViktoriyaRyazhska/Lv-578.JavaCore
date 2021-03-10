ee391b3552ca0e6440e08442bb3290dc0ea476d3
unicorn111 <martha.pryshlyak1999@gmail.com>
2021-03-10 22:30:13 +0200

    fixed

--- homework2/Person.java
+++ homework2/Person.java
@@ -4,18 +4,15 @@ import java.io.InputStreamReader;
 import java.time.Year;
 
 public class Person {
-	private String firstName = null;
-	private String lastName = null;
+	private String firstName;
+	private String lastName;
 	private int birthYear;
 	
-	public Person() throws IOException {
-		this.input();
-	}
+	public Person() {}
 	
-	public Person(String firstName, String lastName) throws IOException {
+	public Person(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.input();
 	}
 	
 	public String getFirstName() {
@@ -47,15 +44,11 @@ public class Person {
 		return currYear - this.birthYear;
 	}
 	
-	public void input() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Hi, there! ");
-		if(firstName == null) {
-			System.out.println("Please, enter the first name:");
-			this.firstName = br.readLine();
-			System.out.println("Please, enter the last name:");
-			this.lastName = br.readLine();
-		}
+	public void input(BufferedReader br) throws IOException {
+		System.out.println("Hi, there! Please, enter the first name:");
+		this.firstName = br.readLine();
+		System.out.println("Please, enter the last name:");
+		this.lastName = br.readLine();
 		System.out.println("Please, enter the year of birth:");
 		this.birthYear = Integer.parseInt(br.readLine());
 	}
@@ -71,11 +64,22 @@ public class Person {
 	}
 	
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Person p1 = new Person();
 		Person p2 = new Person();
 		Person p3 = new Person("Jack", "Black");
 		Person p4 = new Person("John", "Smith");
 		Person p5 = new Person("Kate", "Brown");
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		System.out.println(p4);
+		System.out.println(p5);
+		p1.input(br);
+		p2.input(br);
+		p3.input(br);
+		p4.setBirthYear(1977);
+		p5.setBirthYear(2001);
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
