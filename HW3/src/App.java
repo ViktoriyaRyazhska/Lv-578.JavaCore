718f95a8af733a00dde62458d852fdbdf84b4525
Tsisar <fhantom.tp@gmail.com>
2021-03-11 16:12:47 +0200

    fix bufferedReader

--- HW3/src/App.java
+++ HW3/src/App.java
@@ -5,11 +5,11 @@ public class App {
 
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		for (int i = 1; i < 6; i++){
 		    System.out.printf("\nPlease enter information about person %d. First name, last name and birth year, for example \"Vasia Pupkin 1990\".\n", i);
-		    Person person = new Person();
-			person.input(new BufferedReader(new InputStreamReader(System.in)));
+		    Person person = new Person(bufferedReader);
+			person.input();
 			person.output();
 		}
 	}
